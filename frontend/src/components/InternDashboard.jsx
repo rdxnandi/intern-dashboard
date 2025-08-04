abf654dd4531f9cbcf899cf471de9238af48728c
Riktam Nandi <117157642+rdxnandi@users.noreply.github.com>
2025-08-04 20:43:03 +0530

    Update InternDashboard.jsx

--- frontend/src/components/InternDashboard.jsx
+++ frontend/src/components/InternDashboard.jsx
@@ -6,7 +6,7 @@ const InternDashboard = () => {
   const [intern, setIntern] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/intern")
+    fetch("https://intern-dashboard-backend-8idy.onrender.com/api/intern")
       .then((res) => res.json())
       .then((data) => setIntern(data))
       .catch((err) => console.log("Intern fetch error:", err));
